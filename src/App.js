2d8b5aea60bb1448ac7d9d7aebcded7959957306
PlamenCode <plamenggg233@gmail.com>
2023-12-19 20:02:53 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import './App.css';
+import { useState } from 'react';
 import { Box, Container } from '@chakra-ui/react';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import TextInput from './components/TextInput';
+import KeywordsModal from './components/KeywordsModal';
+
 
 function App() {
+    const [keywords, setKeywords] = useState('');
+    const [isOpen, setIsOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-    function extractKeywords(text){
-        console.log(text);
+    async function extractKeywords(text){
+        setLoading(true);
+        setIsOpen(true);
+        console.log(process.env.OPEN_AI_API_KEY);
+        console.log(process.env.OPEN_AI_API_URL);
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${process.env.OPEN_AI_API_KEY}`
+            },
+            body: JSON.stringify({
+                model: 'gpt-3.5-turbo',
+                prompt: `Extract keywords from this text. Make the first letter of each word uppercase and separate with commas\n\n` + text + '',
+                temperature: 0.5,
+                max_token: 60,
+                frequency_penalty: 0.8
+            })
+        };
+
+        const response = await fetch(process.env.OPEN_AI_API_URL, options);
+        const resultJson = await response.json();
+        const data = resultJson.choices[0].text.trim();
+        console.log(data);
+
+        setKeywords(data)
+        setLoading(false);
     };
+
+    function closeModal(){
+        setIsOpen(false);
+    }
+
   return (
     <Box bg='blue.600' color='white' height='100vh' paddingTop='130px' >
         <Container maxWidth='3xl' centerContent >
@@ -16,6 +54,7 @@ function App() {
             <TextInput extractKeywords={extractKeywords}/>
             <Footer />
         </Container>
+        <KeywordsModal keywords={keywords} loading={loading} isOpen={isOpen} closeModal={closeModal} />
     </Box>
   );
 }
