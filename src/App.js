404a1016348aacbd07f34288cae91826758817f4
PlamenCode <plamenggg233@gmail.com>
2023-12-19 18:43:18 +0200

    input component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import { Box, Container } from '@chakra-ui/react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import TextInput from './components/TextInput';
 
 function App() {
+
+    function extractKeywords(text){
+        console.log(text);
+    };
   return (
     <Box bg='blue.600' color='white' height='100vh' paddingTop='130px' >
         <Container maxWidth='3xl' centerContent >
             <Header />
-
+            <TextInput extractKeywords={extractKeywords}/>
             <Footer />
         </Container>
     </Box>
--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,9 +1,45 @@
-import React from 'react';
-import {  } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { Button, Textarea, useToast } from '@chakra-ui/react';
 
 
-export default function TextInput() {
+export default function TextInput({ extractKeywords }) {
+    const [text, setText] = useState('');
+    const toast = useToast();
+
+    function submitText(){
+        if(text === ''){
+            toast({
+                title: 'Text field is empty',
+                description: 'Please enter some text in the text field.',
+                status: 'error',
+                duration: 4000,
+                isClosable: true
+            })
+        } else{
+            extractKeywords(text);
+        }
+    };
+
   return (
-    <div>TextInput</div>
+    <>
+        <Textarea 
+            bg='blue.400' 
+            color='white' 
+            padding='4px' 
+            marginTop='6px' 
+            height='200px'
+            value={text}
+            onChange={ (e) => setText(e.target.value) }
+        />
+
+        <Button 
+            bg='blue.500'
+            color='white'
+            marginTop='4px'
+            width='100%'
+            _hover={{ bg: 'blue.700' }} 
+            onClick={ submitText }
+        > Extract Keywords</Button>
+    </>
   )
 }
